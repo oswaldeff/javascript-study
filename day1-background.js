45fb34aabb3032ce83466bcd1f5240c5782b5fe1
owaldeff <tialem94@gmail.com>
2023-03-04 23:43:31 +0900

    feat: hello javascript

---
+++ day1-background.js
@@ -0,0 +1,8 @@
+// Browser REPL
+// 브라우저의 코드를 한줄씩 Read, 받은 입력을 평가 Eval, 결과를 출력 Print, 출력 이후 프롬프트에서 새로운 입력을 대기하는 과정 Loop
+
+// 세미콜론을 붙인다;
+console.log('Hello World');
+
+// 명령어에 대한 구분이 가능하다
+console.log('Paris'); console.log('Seoul'); console.log('Tokyo');
