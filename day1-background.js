291b7599cc129e450a1bdf56ac2d2594692a1517
owaldeff <tialem94@gmail.com>
2023-03-05 21:25:41 +0900

    feat: 문자열 기본

--- day1-background.js
+++ day1-background.js
@@ -6,3 +6,19 @@ console.log('Hello World');
 
 // 명령어에 대한 구분이 가능하다
 console.log('Paris'); console.log('Seoul'); console.log('Tokyo');
+
+// ` 백틱은 문자열 선언시 줄바꿈이 가능하다
+`줄바꿈
+공백 `
+
+// '' 작은따옴표 줄바꿈 \n
+'줄바꿈\n'
+
+// 타입확인 typeof
+typeof '문자열'; // 출력결과 string
+
+// 작은따옴표+역슬래쉬 = 작은따옴표 표기
+'That\'s right' // 출력결과 "That's right"
+
+// 슬래쉬 출력
+'True // False' // 출력결과 'True / False'
