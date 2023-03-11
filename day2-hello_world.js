01fd0d8c597bf346568fe65330310aa41b45a2d1
owaldeff <tialem94@gmail.com>
2023-03-11 23:23:44 +0900

    feat: 스크립트 기초

---
+++ day2-hello_world.js
@@ -0,0 +1,16 @@
+<script>
+
+{/* script `type` is optional */}
+<script type="text/javascript"></script>
+
+{/* external script through `path` */}
+<script src="day1-background.js"></script>
+
+{/* src속성이 사용된다면 내부의 속성은 무시된다. */}
+<script src="day1-background.js">here</script>
+
+1. 자바스크립트를 사용하기 위해서 <script></script> 태그사용
+2. script에서 type과 languange를 명시하는건 optional
+3. src를 통해 외부 file접근 및 내부의 속성은 무시된다.
+
+</script>
