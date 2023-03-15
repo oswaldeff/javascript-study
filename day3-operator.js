acc4900ac1aff327f78514f04ce6a955320b5eb4
owaldeff <tialem94@gmail.com>
2023-03-15 22:22:53 +0900

    fix: 숫자 연산자

--- day3-operator.js
+++ day3-operator.js
@@ -10,3 +10,22 @@
 
 5/2 // 출력결과 2.5
 
+// 숫자
+
+typeof 0x1a1 // 출력결과 number타입
+typeof NaN // 출력결과 number타입
+
+"123" + 4 // 출력결과 127이 아닌 "1234"
+
+parseInt("123") + 4 // 출력결과 127
+typeof parseInt("123") // 출력결과 number타입
+
+Number("123") + 4 // 출력결과 127
+typeof Number("123") // 출력결과 number타입
+
+parseFloat("3.14") // 출력결과 3.14
+typeof parseFloat("3.14") // 출력결과 number타입
+
+Number("3.14") // 출력결과 3.14 !Number은 소수도 알아서 숫자로 바꿔준다!
+parseInt("3월") // 출력결과 3 -> parseInt는 문자열에서 숫자를 parsing
+Number("3월") // 출력결과 NaN -> Number은 문자열에서 숫자를 parsing하지 못하고 NaN을 return
