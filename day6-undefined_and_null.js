18d156680571c982a5cc34e27ad3d4c3d124fc8f
owaldeff <tialem94@gmail.com>
2023-03-21 21:54:24 +0900

    feat: 빈 값

---
+++ day6-undefined_and_null.js
@@ -0,0 +1,11 @@
+// 빈값 사용하기
+typeof undefined; // 출력결과 undefined
+
+// 비교
+undefined == null; // 출력결과 true
+
+// 자료형까지 비교
+undefined === null; // 출력결과 false
+
+// javascript 버그
+typeof null; // 출력결과 "object"자료형
