a6b2c3d26265a3b4d88b1e7c943165fbdddabd70
owaldeff <tialem94@gmail.com>
2023-03-27 22:20:38 +0900

    feat: 반복문

---
+++ day11-loop.js
@@ -0,0 +1,23 @@
+// loops
+let i = -3;
+while (i < 0) {
+    console.log(`start value: ${i}`);
+    i++;
+    console.log(`end value: ${i}`);
+}
+
+console.log('======================');
+
+// do while
+do {
+    console.log(`start value: ${i}`);
+    i++;
+    console.log(`end value: ${i}`);
+} while (i < 0);
+
+console.log('~~~~~~~~~~~~~~~~~~~~~~~');
+
+// for loop
+for (i=3; i>0; i--) {
+    console.log(i);
+}
